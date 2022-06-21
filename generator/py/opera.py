00e93bffac5b6ae2be6872f294bda36bafd5103a
caseyanderson <cta@caseyanderson.com>
2022-06-20 18:35:13 -0700

    adds categorizePOS() to Opera class, renames getPOS() getTagsPOS()

--- generator/py/opera.py
+++ generator/py/opera.py
@@ -1,12 +1,13 @@
-from time import sleep
-from datetime import datetime
-from pathlib import Path
-import os
-import itertools
-
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk import pos_tag
+
+from itertools import chain
+
+from datetime import datetime
+from pathlib import Path
+
+from os import chdir, mkdir
 
 import argparse
 
@@ -32,7 +33,7 @@ class Opera:
         for sentence in self.sentences:
             if preserveSpaces == True:
                 words = [[word_tokenize(w), ' '] for w in sentence.split()]
-                wordList = list(itertools.chain(*list(itertools.chain(*words))))
+                wordList = list(chain(*list(chain(*words))))
                 if wordList[-1] == ' ':
                     # removes trailing whitespace @ end of sentence if there is any
                     wordList.pop()
@@ -41,18 +42,19 @@ class Opera:
                 words = word_tokenize(sentence)
                 self.words.append(words)
         self.preserveSpaces = preserveSpaces
-    
-    def getPOS(self):
+
+
+    def getTagsPOS(self):
         """ parts of speech analysis
             TODO:
-                1. if preserveSpaces
+                1. if preserveSpaces == False...
         """
         self.processedCorpus = []
         
         if self.preserveSpaces == False:
-            print("removing spaces")
+            print('removing spaces')
         elif self.preserveSpaces == True:
-            # print("preserving spaces")
+            # print('preserving spaces')
             sentenceStep = 0
             processedSentences = []
             for sentence in self.sentences:
@@ -62,11 +64,32 @@ class Opera:
                 for word in self.words[sentenceStep]:
                     if word.isspace() == False:
                         posWord = list(posSentence.pop(0))
-                        processedSentence.append([posWord[0], {"pos": posWord[1]}])
+                        processedSentence.append([posWord[0], {'tagPOS': posWord[1]}])
                     elif word.isspace() == True:
                         processedSentence.append(word)
                 self.processedCorpus.append(processedSentence)
                 sentenceStep+=1
-                # print("~~~ no more words in sentence ~~~")
-            # print("*** no more sentences in corpus ***")
+                # print('~~~ no more words in sentence ~~~')
+            # print('*** no more sentences in corpus ***')
             # print()
+
+
+    def categorizePOS(self, category, posSymbols):
+        category = category
+        posSymbols = posSymbols
+        for symbol in posSymbols:
+            sentenceCounter = 0
+            for sentence in self.processedCorpus:
+                wordCounter = 0
+                for word in sentence:
+                    if type(word) is list:
+                        if word[1]['tagPOS'] == symbol:
+                            self.processedCorpus[sentenceCounter][wordCounter][1]['categoryPOS'] = category
+                    wordCounter+=1
+                sentenceCounter+=1
+
+
+    def syleCategoriesPOS4Layer(self, category):
+        '''
+        
+        '''
